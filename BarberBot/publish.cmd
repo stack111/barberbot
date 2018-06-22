93e1688ee72f2868b764b60d5127f8cd285eb59b
Daniel Stack <dstack@microsoft.com>
2018-06-21 22:37:10 -0700

    initial commit

---
+++ BarberBot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.FormBot.sln -p:DeployOnBuild=true -p:PublishProfile=barberbottest-Web-Deploy.pubxml -p:Password=04Wpmkt6WujkjDm76my1mx750sNcwwEcg1sjhGxi8mPluGL3iF5xqEvcswnj
+
