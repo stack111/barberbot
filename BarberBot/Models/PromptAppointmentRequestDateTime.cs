b8ea504be1dfe7b792422d1de6a376a49c96f3fd
Daniel Stack <dstack@microsoft.com>
2018-07-19 15:43:50 -0700

    suggestions

--- BarberBot/Models/PromptAppointmentRequestDateTime.cs
+++ BarberBot/Models/PromptAppointmentRequestDateTime.cs
@@ -7,7 +7,6 @@
     using Microsoft.Bot.Builder.Dialogs;
     using Microsoft.Bot.Builder.Dialogs.Internals;
     using Microsoft.Bot.Connector;
-    using Microsoft.Recognizers.Text.Choice;
     using Microsoft.Recognizers.Text.DateTime;
 
     [Serializable]
@@ -15,15 +14,17 @@
     {
         readonly DateTime PSTDate = DateTime.UtcNow.AddHours(-7).Date;
         readonly DateTime PSTDateTime = DateTime.UtcNow.AddHours(-7);
-        private readonly AppointmentRequest request;
+        readonly AppointmentRequest request;
+        readonly int MaxNumberOfSuggestionAttempts = 3;
         string timeValue;
-        bool hasAttempted;
+        private int suggestionAttemptCounter;
         AppointmentRequest suggestedAppointmentRequest;
         
         public PromptAppointmentRequestDateTime(AppointmentRequest request, string prompt, string retry = null, string tooManyAttempts = null, int attempts = 3)
           : base(new PromptOptions<string>(prompt, retry, tooManyAttempts, attempts: attempts))
         {
             this.request = request;
+            suggestionAttemptCounter = 0;
         }
 
         protected override bool TryParse(IMessageActivity message, out AppointmentRequest result)
@@ -57,66 +58,21 @@
                 var message = await activity;
                 var quitCondition = message.Text.ToLower().Contains("cancel");
 
-                if (hasAttempted)
-                {
-                    var choiceModels = ChoiceRecognizer.RecognizeBoolean(message.Text, message.Locale);
-                    if (!choiceModels.Any())
-                    {
-                        request.ResetDateTime();
-                        await context.PostAsync("Sorry, I didn't understand you, can you try again with the appointment date and time?", message.Locale);
-                        hasAttempted = false;
-                        context.Wait(MessageReceivedAsync);
-                        return;
-                    }
-                    else
-                    {
-                        bool confirmation = (bool)choiceModels[0].Resolution["value"];
-                        double score = (double)choiceModels[0].Resolution["score"];
-                        if (score <= .5)
-                        {
-                            var promptSuggestionConfirmation = new PromptDialog.PromptConfirm(
-                                $"Sorry, I didn't get that, can you confirm you are ok with the suggested appointment?",
-                                "Sorry I didn't understand you. Can you choose an option below?",
-                                2);
-                            context.Call(promptSuggestionConfirmation, this.AfterSuggestionConfirmation);
-                            return;
-                        }
-                        else if (confirmation)
-                        {
-                            await context.PostAsync("Great, we'll use that appointment.", message.Locale);
-                            hasAttempted = false;
-                            request.CopyFrom(suggestedAppointmentRequest);
-                            context.Done(request);
-                            return;
-                        }
-                        else
-                        {
-                            request.ResetDateTime();
-                            await context.PostAsync("Ok, can you suggest another day and time which works for you?", message.Locale);
-                            hasAttempted = false;
-                            context.Wait(MessageReceivedAsync);
-                            return;
-                        }
-                    }
-                }
-
                 if (quitCondition)
                 {
                     request.ResetDateTime();
-                    hasAttempted = false;
                     context.Done(request);
                 }
                 else
                 {
                     var recognizer = new DateTimeRecognizer(message.Locale, DateTimeOptions.None, false);
                     var dateTimeModel = recognizer.GetDateTimeModel();
-                    var results = dateTimeModel.Parse(message.Text, PSTDate);
+                    var results = dateTimeModel.Parse(message.Text, PSTDateTime);
 
                     if (!results.Any())
                     {
                         request.ResetDateTime();
-                        await context.PostAsync("Sorry, I didn't understand you, can you try again?", message.Locale);
-                        hasAttempted = false;
+                        await context.PostAsync("Sorry, I didn't understand you, can you try again with a date and time?", message.Locale);
                         context.Wait(MessageReceivedAsync);
                         return;
                     }
@@ -133,7 +89,7 @@
                         if (response.IsAvailable)
                         {
                             var promptTodayConfirmation = new PromptDialog.PromptConfirm(
-                                $"I assumed today and found {timeValue}, everything looks available at the moment but I need you to confirm that's what you meant and not a different day.",
+                                $"I assumed today and found {timeValue}, everything looks available at the moment but can confirm you that's what you meant?",
                                 "Sorry I didn't understand you. Can you choose an option below?",
                                 2);
                             context.Call(promptTodayConfirmation, this.AfterTodayConfirmation);
@@ -142,9 +98,11 @@
                         {
                             // we will send the suggestion so if the user confirms we can use it.
                             suggestedAppointmentRequest = response.SuggestedRequest;
-                            await context.PostAsync(response.FormattedErrorMessage(), message.Locale);
-                            hasAttempted = true;
-                            context.Wait(this.MessageReceivedAsync);
+                            var promptSuggestionConfirmation = new PromptDialog.PromptConfirm(
+                               response.FormattedErrorMessage(),
+                               "Sorry I didn't understand you. Can you choose an option below?",
+                               2);
+                            context.Call(promptSuggestionConfirmation, this.AfterSuggestionConfirmation);
                         }
                     }
                     else if (modelResult["type"] == "datetime")
@@ -156,16 +114,17 @@
 
                         if (response.IsAvailable)
                         {
-                            hasAttempted = false;
                             await context.PostAsync("Great, that will work.", message.Locale);
                             context.Done(request);
                         }
                         else
                         {
                             suggestedAppointmentRequest = response.SuggestedRequest;
-                            await context.PostAsync(response.FormattedErrorMessage(), message.Locale);
-                            hasAttempted = true;
-                            context.Wait(this.MessageReceivedAsync);
+                            var promptSuggestionConfirmation = new PromptDialog.PromptConfirm(
+                               response.FormattedErrorMessage(),
+                               "Sorry I didn't understand you. Can you choose an option below?",
+                               2);
+                            context.Call(promptSuggestionConfirmation, this.AfterSuggestionConfirmation);
                         }
                     }
                     else if (modelResult["type"] == "date")
@@ -176,26 +135,33 @@
                         if (response.IsAvailable)
                         {
                             var promptTodayConfirmation = new PromptDialog.PromptConfirm(
-                               $"I found around {request.RequestedDateTime.ToShortTimeString()}, everything looks available at the moment but I need you to confirm that's what you meant at this time not another time.",
+                               $"I found around {request.RequestedDateTime.ToShortTimeString()}, everything looks available at the moment but can you confirm that's what you meant?",
                                "Sorry I didn't understand you. Can you choose an option below?",
                                2);
                             context.Call(promptTodayConfirmation, this.AfterTimeConfirmation);
                         }
                         else
                         {
-                            await context.PostAsync($"I guessed around {request.RequestedDateTime.ToShortTimeString()}", context.Activity.AsMessageActivity().Locale);
+                            await context.PostAsync($"I looked around {request.RequestedDateTime.ToShortTimeString()}", context.Activity.AsMessageActivity().Locale);
                             suggestedAppointmentRequest = response.SuggestedRequest;
-                            await context.PostAsync(response.FormattedErrorMessage().Replace("Sorry", "But sorry"), message.Locale);
-                            hasAttempted = true;
-                            context.Wait(this.MessageReceivedAsync);
+                            var promptSuggestionConfirmation = new PromptDialog.PromptConfirm(
+                               response.FormattedErrorMessage().Replace("Sorry", "But sorry"),
+                               "Sorry I didn't understand you. Can you choose an option below?",
+                               2);
+                            context.Call(promptSuggestionConfirmation, this.AfterSuggestionConfirmation);
                         }
+                    }
+                    else
+                    {
+                        request.ResetDateTime();
+                        context.Done(request);
                     }
                 }
             }
-            catch (TooManyAttemptsException ex)
+            catch (TooManyAttemptsException)
             {
                 request.ResetDateTime();
-                context.Fail(ex);
+                context.Done(request);
             }
         }
 
@@ -205,14 +171,13 @@
             if (confirmed)
             {
                 await context.PostAsync("Ok", context.Activity.AsMessageActivity().Locale);
-                hasAttempted = false;
+                suggestionAttemptCounter = 0;
                 context.Done(request);
             }
             else
             {
                 await context.PostAsync("Ok, let's try again and you can tell me the date and time. Or you can exit with 'cancel'.", context.Activity.AsMessageActivity().Locale);
-                hasAttempted = false;
-                context.Wait(MessageReceivedAsync);
+                DecideToContinueSuggestions(context, request);
             }
         }
 
@@ -222,17 +187,16 @@
             if (confirmation)
             {
                 await context.PostAsync("Great, we'll use that appointment.", context.Activity.AsMessageActivity().Locale);
-                hasAttempted = false;
                 request.CopyFrom(suggestedAppointmentRequest);
+                suggestionAttemptCounter = 0;
                 context.Done(request);
             }
             else
             {
                 request.ResetDateTime();
-                await context.PostAsync("Ok, can you suggest another day and time which works for you?", context.Activity.AsMessageActivity().Locale);
-                hasAttempted = false;
-                context.Wait(MessageReceivedAsync);
-                context.Done(request);
+                await context.PostAsync("Ok, can you suggest another day and time which works for you? Or you can exit with 'cancel'.", context.Activity.AsMessageActivity().Locale);
+
+                DecideToContinueSuggestions(context, request);
             }
         }
 
@@ -247,14 +211,29 @@
                 
                 request.RequestedDateTime = todayDate;
                 await context.PostAsync("Ok", context.Activity.AsMessageActivity().Locale);
-                hasAttempted = false;
+                suggestionAttemptCounter = 0;
                 context.Done(request);
             }
             else
             {
                 await context.PostAsync("Ok, let's try again and you can tell me the date and time. Or you can exit with 'cancel'.", context.Activity.AsMessageActivity().Locale);
-                hasAttempted = false;
+                suggestionAttemptCounter++;
+                DecideToContinueSuggestions(context, request);
+            }
+        }
+
+        private void DecideToContinueSuggestions(IDialogContext context, AppointmentRequest request)
+        {
+            if (suggestionAttemptCounter < MaxNumberOfSuggestionAttempts)
+            {
+                suggestionAttemptCounter++;
                 context.Wait(MessageReceivedAsync);
+            }
+            else
+            {
+                suggestionAttemptCounter = 0;
+                request.ResetDateTime();
+                context.Done(request);
             }
         }
     }
