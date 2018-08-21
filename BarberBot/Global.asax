b3c96e002c9274a712f9a038c1b733acb26ef322
Daniel Stack <dstack@microsoft.com>
2018-08-20 23:42:39 -0700

    Cleaning up old asp web api 2 proj
    we don't need to support web api 2 when we have asp net core web projects now.

--- BarberBot/Global.asax
+++
@@ -1,1 +0,0 @@
-<%@ Application CodeBehind="Global.asax.cs" Inherits="BarberBot.WebApiApplication" Language="C#" %>
--- BarberBot/publish.cmd
+++
@@ -1,3 +0,0 @@
-nuget restore
-msbuild Microsoft.Bot.Sample.FormBot.sln -p:DeployOnBuild=true -p:PublishProfile=barberbottest-Web-Deploy.pubxml -p:Password=04Wpmkt6WujkjDm76my1mx750sNcwwEcg1sjhGxi8mPluGL3iF5xqEvcswnj
-
